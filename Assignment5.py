b0603b69571256d72cf6ad8f1391cc29ed2096f5
anmyany <anmyany@gmail.com>
2020-10-26 00:57:28 -0400

    move3

---
+++ Assignment5.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 02:34:48 2020
+
+@author: anmya
+"""
+
+series = [10,14,8,25,16,22,14,35,15,27,18,40,28,40,28,40,25,65]
+alpha = 0.16221874
+beta = 0.907388102
+gamma = 0.837781263
+n_preds = 8
+slen = 4
+
+def initial_trend(series, slen):
+    sum = 0.0
+    for i in range(slen):
+        sum += float(series[i+slen] - series[i]) / slen
+    return sum / slen
+
+def initial_seasonal_components(series, slen):
+    seasonals = {}
+    season_averages = []
+    n_seasons = int(len(series)/slen)
+    # compute season averages
+    for j in range(n_seasons):
+        season_averages.append(sum(series[slen*j:slen*j+slen])/float(slen))
+    # compute initial values
+    for i in range(slen):
+        sum_of_vals_over_avg = 0.0
+        for j in range(n_seasons):
+            sum_of_vals_over_avg += series[slen*j+i]-season_averages[j]
+        seasonals[i] = sum_of_vals_over_avg/n_seasons
+    return seasonals
+
+def triple_exponential_smoothing(series, slen, alpha, beta, gamma, n_preds):
+    global trend
+    result = []
+    seasonals = initial_seasonal_components(series, slen)
+    for i in range(len(series)+n_preds):
+        if i == 0: # initial values
+            smooth = series[0]
+            trend = initial_trend(series, slen)
+            result.append(series[0])
+            continue
+        if i >= len(series): # we are forecasting
+            m = i - len(series) + 1
+            result.append((smooth + m*trend) + seasonals[i%slen])
+        else:
+            val = series[i]
+            last_smooth, smooth = smooth, alpha*(val-seasonals[i%slen]) + (1-alpha)*(smooth+trend)
+            trend = beta * (smooth-last_smooth) + (1-beta)*trend
+            seasonals[i%slen] = gamma*(val-smooth) + (1-gamma)*seasonals[i%slen]
+            result.append(smooth+trend+seasonals[i%slen])
+    return result
+
+
+
+
+
+
